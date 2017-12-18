e67789c059e5e99110de40ed93140f9542d3d524
Evelina <34212114+Eva4Eva@users.noreply.github.com>
2017-12-18 21:45:56 +0300

    3 неделя
    Реализовать калькулятор в стиле ООП. Архитектуру приложения продумать самостоятельно.

--- task6.java
+++ task6.java
@@ -3,44 +3,38 @@
 
 
 import java.util.Scanner;
-public class Calculator {
-    double num_1;
-    double num_2;
-    double num_3;
-    public static void main(String[] args){
-
-        Calculator object_1 = new Calculator();
-        Calculator object_2 = new Calculator();
-        Calculator object_3 = new Calculator();
-
-Scanner input = new Scanner(System.in);
-        System.out.println("Введите num_1");
- object_1.num_1=input.nextDouble();
-Scanner input_2 = new Scanner(System.in);
-        System.out.println("Введите num_2");
-object_2.num_2=input.nextDouble();
-object_3.num_3=0;
-String choose = new String();
-System.out.println("Введете символ математического действия");
-choose=input_2.nextLine();
-switch (choose){
-
-    case "+": object_3.num_3=object_1.num_1+object_2.num_2;
-    System.out.println(object_3.c);
-    break;
-    case  "-": object_3.num_3=object_1.num_1-object_2.num_2;
-        System.out.println(object_3.num_3);
-        break;
-    case "*": object_3.num_3= object_1.num_1*object_2.num_2;
-    System.out.println(object_3.num_3);
-    break;
-    case "/": object_3.num_3=object_1.num_1/object_2.num_2;
-    System.out.println(object_3.num_3);
-    break;
-    default: System.out.println("Ошибка!Вы ввели неверный символ.");
-}
+public class Object_calculator {
+    double number_1;
+    double number_2;
+    double number_3;
+    public static void main(String[] args) {
+        Object_calculator x = new Object_calculator();
+        Object_calculator y = new Object_calculator();
+        Object_calculator z = new Object_calculator();
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Первое значение");
+        x.number_1 = enter.nextDouble();
+        Scanner enter_2 = new Scanner(System.in);
+        System.out.println("Второе значение");
+        y.number_2 = enter.nextDouble();
+        z.number_3 = 0;
+        String choose = new String();
+        System.out.println("Символ математическоей операции");
+        choose = enter_2.nextLine();
+        if (choose.equals("+")) {
+            z.number_3 = x.number_1 + y.number_2;
+            System.out.println(z.number_3);
+        } else if (choose.equals("-")) {
+            z.number_3 = x.number_1 - y.number_2;
+            System.out.println(z.number_3);
+        } else if (choose.equals("*")) {
+            z.number_3 = x.number_1 * y.number_2;
+            System.out.println(z.number_3);
+        } else if (choose.equals("/")) {
+            z.number_3 = x.number_1 / y.number_2;
+            System.out.println(z.number_3);
+        } else {
+            System.out.println("Ошибка!");
+        }
     }
-
-
-
 }
